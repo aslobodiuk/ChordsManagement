1fac6ae6b8797b4c8e9fdaa10eb1ea6741959622
oleksii <aslobodiuk@gmail.com>
2025-06-18 14:04:50 +0300

    -add error notification -check wrong input

--- chord_editor.py
+++ chord_editor.py
@@ -76,7 +76,10 @@ class ChordEditor:
 
     def input_chord(self, event, index):
         if event.widget.get():
-            self.text_widget.insert(index, f"({event.widget.get()})", "chord")
+            if re.fullmatch(CHORDS_PATTERN, f"({event.widget.get()})") is None:
+                self.root.after(500, lambda: self.show_non_blocking_message(type='error', msg="Wrong input"))
+            else:
+                self.text_widget.insert(index, f"({event.widget.get()})", "chord")
         event.widget.destroy()
 
     def save_file(self):
@@ -120,7 +123,11 @@ class ChordEditor:
         merge_pdf_files(SONGS_FILENAME, tmp_file, SONGS_FILENAME)
         self.root.after(500, self.show_non_blocking_message)
 
-    def show_non_blocking_message(self, msg: str = "Success!", duration: int = 2000):
+    def show_non_blocking_message(self, type: str = 'success', msg: str = "Success", duration: int = 2000):
+        style = {
+            'success': ('#dff0d8', '#3c763d'),
+            'error': ('#f2dede', '#a94442')
+        }
         # Create a popup window
         popup = tk.Toplevel(self.root)
         popup.title("")
@@ -143,7 +150,7 @@ class ChordEditor:
         popup.geometry(f"+{x}+{y}")
 
         # Add message label
-        label = tk.Label(popup, text=msg, font=("Arial", 11), bg="#dff0d8", fg="#3c763d", relief="solid", bd=1)
+        label = tk.Label(popup, text=msg, font=("Arial", 11), bg=style[type][0], fg=style[type][1], relief="solid", bd=1)
         label.pack(expand=True, fill="both", padx=10, pady=10)
 
         # Auto-destroy after duration (e.g., 2000 ms)
