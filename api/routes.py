c2cd0219fff7e528b70093f10319fcbf12828699
oleksii <aslobodiuk@gmail.com>
2025-06-20 17:55:59 +0300

    - documentation update

--- api/routes.py
+++ api/routes.py
@@ -10,9 +10,13 @@ from db import get_session
 from models import Song, Line, SongRead, SongCreate, SongShort, SongIdsRequest
 from utils.pdf_utils import create_pdf_base
 
-router = APIRouter()
+router = APIRouter(tags=["Songs"])
 
-@router.get("/songs", response_model=Union[List[SongRead], List[SongShort]])
+@router.get(
+    path="/songs",
+    response_model=Union[List[SongRead], List[SongShort]],
+    summary="List of songs"
+)
 def read_songs(
         skip: int = 0,
         limit: int = 100,
@@ -25,21 +29,21 @@ def read_songs(
 
         Parameters
         ----------
-        skip : int, optional
-            Number of records to skip for pagination (default is 0).
-        limit : int, optional
-            Maximum number of records to return (default is 100).
-        search : str, optional
-            Search string to filter songs by title or artist (case-insensitive).
-        short : bool, optional
-            If True, returns a shortened version of each song with only id, title, and artist.
-            If False, returns full song details including lines and chords.
-        session : Session, optional
+        `skip`: `int`, `optional`
+            Number of records to skip for pagination (default is 0).\n
+        `limit`: `int`, `optional`
+            Maximum number of records to return (default is 100).\n
+        `search`: `str`, `optional`
+            Search string to filter songs by title or artist (case-insensitive).\n
+        `short`: `bool`, `optional`
+            If `True`, returns a shortened version of each song with only id, title, and artist.
+            If `False`, returns full song details including lines and chords.\n
+        `session`: `Session`
             Database session dependency.
 
         Returns
         -------
-        Union[List[SongRead], List[SongShort]]
+        `Union[List[SongRead], List[SongShort]]`
             List of songs matching the query. Full or short schema depending on `short` flag.
     """
     statement = (
@@ -62,48 +66,56 @@ def read_songs(
     songs = session.exec(statement).all()
     return [SongShort.model_validate(song) if short else SongRead.model_validate(song) for song in songs]
 
-@router.get("/songs/{song_id}", response_model=SongRead)
+@router.get(
+    path="/songs/{song_id}",
+    response_model=SongRead,
+    summary="Song details"
+)
 def read_song(song_id: int, session: Session = Depends(get_session)):
     """
         Retrieve a single song by its ID.
 
         Parameters
         ----------
-        song_id : int
-            Unique identifier of the song to retrieve.
-        session : Session, optional
+        `song_id` : `int`
+            Unique identifier of the song to retrieve.\n
+        `session` : `Session`
             Database session dependency.
 
         Returns
         -------
-        SongRead
+        `SongRead`
             The song data including all lines and chords.
 
         Raises
         ------
-        HTTPException
-            If no song with the given ID is found (404 Not Found).
+        `HTTPException`
+            If no song with the given ID is found (`404 Not Found`).
     """
     song = session.get(Song, song_id)
     if not song:
         raise HTTPException(status_code=404, detail="Song not found")
     return song
 
-@router.post("/songs", response_model=SongRead)
+@router.post(
+    path="/songs",
+    response_model=SongRead,
+    summary="Song creation"
+)
 def create_song(song_in: SongCreate, session: Session = Depends(get_session)):
     """
         Create a new song from raw input data.
 
         Parameters
         ----------
-        song_in : SongCreate
-            Input data containing title, artist, and raw lyrics string.
-        session : Session, optional
+        `song_in` : `SongCreate`
+            Input data containing title, artist, and raw lyrics string.\n
+        `session` : `Session`
             Database session dependency.
 
         Returns
         -------
-        SongRead
+        `SongRead`
             The created song with all parsed lines and chords.
 
         Notes
@@ -117,27 +129,31 @@ def create_song(song_in: SongCreate, session: Session = Depends(get_session)):
     session.refresh(song)
     return song
 
-@router.post("/songs/to_pdf")
+@router.post(
+    path="/songs/to_pdf",
+    response_model=None,
+    summary="Export songs to PDF"
+)
 def export_to_pdf(request: SongIdsRequest, session: Session = Depends(get_session)):
     """
         Export selected songs to a PDF.
 
         Parameters
         ----------
-        request : SongIdsRequest
-            Object containing list of song IDs.
-        session : Session
+        `request` : `SongIdsRequest`
+            Object containing list of song IDs. If list is `empty` - returns `all` songs\n
+        `session` : `Session`
             Database session.
 
         Returns
         -------
-        StreamingResponse
+        `StreamingResponse`
             PDF document with selected songs.
 
         Raises
         ------
-        HTTPException
-            If no songs are found for the given IDs.
+        `HTTPException`
+            If no songs are found for the given IDs (`404 Not Found`).
     """
     statement = select(Song)
     if request.song_ids:
