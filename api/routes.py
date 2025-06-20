3d63d72823dd3fd0dd521c96c61aadfe895365f5
oleksii <aslobodiuk@gmail.com>
2025-06-20 14:33:57 +0300

    - read songs api's

--- api/routes.py
+++ api/routes.py
@@ -1,6 +1,35 @@
-from fastapi import APIRouter
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import selectinload
+from sqlmodel import Session, select
+
+from db import get_session
+from models import Song, Line, SongRead
+
 router = APIRouter()
 
 @router.get("/")
 def read_root():
     return {"message": "FastAPI is running!"}
+
+@router.get("/songs", response_model=List[SongRead])
+def read_songs(skip: int = 0, limit: int = 100, session: Session = Depends(get_session)):
+    statement = (
+        select(Song)
+        .offset(skip)
+        .limit(limit)
+        .options(
+            selectinload(Song.lines).selectinload(Line.chords)
+        )
+    )
+    songs = session.exec(statement).all()
+    return songs
+
+
+@router.get("/songs/{song_id}", response_model=SongRead)
+def read_song(song_id: int, session: Session = Depends(get_session)):
+    song = session.get(Song, song_id)
+    if not song:
+        raise HTTPException(status_code=404, detail="Song not found")
+    return song
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 from sqlmodel import SQLModel, Field, Relationship
 
@@ -23,4 +23,21 @@ class Chord(SQLModel, table=True):
     chord: str
     line_id: int = Field(default=None, foreign_key="line.id")
     line: Line = Relationship(back_populates="chords")
+
+
+class ChordRead(SQLModel):
+    id: int
+    position: int
+    chord: str
+
+class LineRead(SQLModel):
+    id: int
+    line: str
+    chords: List[ChordRead] = []
+
+class SongRead(SQLModel):
+    id: int
+    title: str
+    artist: str
+    lines: List[LineRead] = []
 
