4b76bef9f0098055c9b3c9748a4b68375177434b
oleksii <aslobodiuk@gmail.com>
2025-06-23 22:43:09 +0300

    - init files - option to run only api

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import threading
 
+from pydantic.v1 import BaseSettings
+
 from gui.chord_editor import run_gui
 from server import run_server
 
+class Settings(BaseSettings):
+    RUN_GUI: bool = False
+
+    class Config:
+        env_file = ".env"
 
 def main() -> None:
-    # Start FastAPI server in a thread
-    server_thread = threading.Thread(target=run_server, daemon=True)
-    server_thread.start()
+    settings = Settings()
 
-    # Run tkinter app
-    run_gui()
+    if settings.RUN_GUI:
+        # Start FastAPI server in a thread
+        server_thread = threading.Thread(target=run_server, daemon=True)
+        server_thread.start()
+
+        # Run tkinter app
+        run_gui()
+
+    run_server()
 
 if __name__ == "__main__":
     main()
